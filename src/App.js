ec633438d2e8d5ee1a24e549db823aba32a94ec2
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-01 16:38:07 +0100

    Created Sum component

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={1} valueB={2} />;
+}
+
+function Sum({ valueA, valueB }) {
+  const result = valueA + valueB;
+  return (
+    <h1>
+      {valueA} + {valueB} = {result};
+    </h1>
+  );
 }
